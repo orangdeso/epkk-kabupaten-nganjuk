f30a0a0542d554a664a2ed5cb92f454d70e6966c
orangdeso <ahmadzaqi98mmm@gmail.com>
2024-08-04 11:06:34 +0700

    Update delete assets icons

--- lib/welcome/presentation/welcome_screen.dart
+++ lib/welcome/presentation/welcome_screen.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:flutter_svg/svg.dart';
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({super.key});
